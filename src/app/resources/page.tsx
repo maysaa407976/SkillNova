a16facab02affa4124dead2f5e8f93bcc5635483
maysaa407976 <38818909+maysaa407976@users.noreply.github.com>
2026-07-11 15:36:37 +0300

    Update page.tsx

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,1 +1,5 @@
-export default function Page(){return <main style={padding:24}><h1>resources</h1></main>}
+export default function Page(){return  <main>
+      <h1>Course Files</h1>
+    </main>
+  );
+}
